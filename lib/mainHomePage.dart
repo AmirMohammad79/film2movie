b72d6be48af7cc9ec2edb137f53f3cb247d4d7ed
amir mohammad <a.abdpour0881@gmail.com>
2024-04-10 19:52:52 +0330

    Check out the news section of the site

--- lib/mainHomePage.dart
+++ lib/mainHomePage.dart
@@ -153,6 +153,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 height: 8,
               ),
               Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   //Left side of site
                   Column(
@@ -231,14 +232,288 @@ class _MyHomePageState extends State<MyHomePage> {
                       CategoryBox(
                           title: 'لینک های مهم',
                           texts: [
-                     ' دانلود رایگان فیلم',
-                     ' دانلود سریال عاشقانه قسمت ۱۷ هفدهم'
+                            ' دانلود رایگان فیلم',
+                            ' دانلود سریال عاشقانه قسمت ۱۷ هفدهم'
                           ],
                           height: 80,
                           visible: true),
-                      CategoryBox(title: 'تبلیغات', texts: [], height: 16, visible: false)
+                      CategoryBox(
+                          title: 'تبلیغات',
+                          texts: [],
+                          height: 16,
+                          visible: false)
                     ],
                   ),
+                  //movies list side of home page
+                  Column(
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.all(16),
+                            margin: EdgeInsets.only(left: 8, right: 8, top: 8),
+                            width: 900,
+                            height: 80,
+                            decoration: BoxDecoration(
+                              color: Color(0xffEFEEEE),
+                              borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(4),
+                                topLeft: Radius.circular(4),
+                              ),
+                              border: Border.all(color: Colors.grey),
+                            ),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  color: Color(0xff337ab7),
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Padding(
+                                padding: const EdgeInsets.only(right: 8.0),
+                                child: Align(
+                                    alignment: Alignment.centerRight,
+                                    child: Text('آخرین اخبار جهان',
+                                        style: TextStyle(
+                                            fontFamily: 'Far_Dinar_Two_Medium',
+                                            fontSize: 25,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w400))),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: 900,
+                            height: 300,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(4),
+                                  bottomRight: Radius.circular(4),
+                                ),
+                                border: Border.all(color: Colors.grey)),
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Container(
+                                        margin: EdgeInsets.all(8),
+                                        padding: EdgeInsets.all(4),
+                                        color: Colors.grey[50],
+                                        child: Column(
+                                          mainAxisAlignment: MainAxisAlignment.start,
+                                          children: [
+                                            Row(
+                                              children: [
+                                                Text(
+                                                  '۱۵ دی ۱۴۰۰',
+                                                  textDirection: TextDirection.rtl,
+                                                  textAlign: TextAlign.right,
+                                                  style: TextStyle(
+                                                    fontFamily: 'tahoma',
+                                                    fontSize: 12,
+                                                    color: Colors.black,
+                                                  ),
+                                                ),
+                                                SizedBox(width: 2,),
+                                                Icon(
+                                                  Icons.watch_later_outlined,
+                                                  size: 12,
+                                                  color: Colors.black,
+                                                )
+                                              ],
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                      Container(
+                                        child: Row(children: [Column(
+                                          crossAxisAlignment:
+                                          CrossAxisAlignment.end,
+                                          children: <Widget>[
+                                            Text(
+                                              '۲۰ فیلم برتر سال ۲۰۲۱ به انتخاب فیلم تو مووی',
+                                              textDirection: TextDirection.rtl,
+                                              textAlign: TextAlign.right,
+                                              style: TextStyle(
+                                                fontFamily: 'tahoma',
+                                                fontSize: 12,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                            SizedBox(
+                                              height: 6,
+                                            ),
+                                            Row(
+                                              mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                              children: [
+                                                Icon(
+                                                  Icons
+                                                      .keyboard_double_arrow_left_rounded,
+                                                  color: Color(0xff337ab7),
+                                                  size: 9,
+                                                ),
+                                                TextButton(
+                                                    onPressed: () {},
+                                                    child: Text(
+                                                      'مشاهده ادامه متن',
+                                                      style: TextStyle(
+                                                        fontFamily: 'tahoma',
+                                                        fontSize: 9,
+                                                        color: Color(0xff337ab7),
+                                                      ),
+                                                    )),
+                                              ],
+                                            )
+                                          ],
+                                        ),
+                                          Container(
+                                            margin: EdgeInsets.all(8),
+                                            width: 250,
+                                            height: 125,
+                                            decoration: BoxDecoration(
+                                                borderRadius:
+                                                BorderRadius.circular(16),
+                                                image: DecorationImage(
+                                                  fit: BoxFit.fitWidth,
+                                                  image: AssetImage(
+                                                    'assets/images/1 (2).png',
+                                                  ),
+                                                )),
+                                          ),],),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Divider(color: Colors.grey,),
+                                Container(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Container(
+                                            margin: EdgeInsets.all(8),
+                                            padding: EdgeInsets.all(4),
+                                            color: Colors.grey[50],
+                                            child: Column(
+                                              mainAxisAlignment: MainAxisAlignment.start,
+                                              children: [
+                                                Row(
+                                                  children: [
+                                                    Text(
+                                                      '۱۵ دی ۱۴۰۰',
+                                                      textDirection: TextDirection.rtl,
+                                                      textAlign: TextAlign.right,
+                                                      style: TextStyle(
+                                                        fontFamily: 'tahoma',
+                                                        fontSize: 12,
+                                                        color: Colors.black,
+                                                      ),
+                                                    ),
+                                                    SizedBox(width: 2,),
+                                                    Icon(
+                                                      Icons.watch_later_outlined,
+                                                      size: 12,
+                                                      color: Colors.black,
+                                                    )
+                                                  ],
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                          SizedBox(height: 62.5,),
+                                          Container(
+                                            margin: EdgeInsets.only(left:38 ),
+                                            width: 120,
+                                            height: 40,
+                                            decoration: BoxDecoration(
+                                                color: Color(0xff1D943C),
+                                                borderRadius: BorderRadius.only(
+                                                  topLeft: Radius.circular(16),
+                                                  topRight: Radius.circular(16),
+                                                )
+                                            ),
+                                            child: Center(
+                                              child: Text('آرشیو آخبار' , style: TextStyle(
+                                                  fontFamily: 'Far_Dinar_Two_Medium',
+                                                  fontSize: 15,
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.w400)),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                      Container(
+                                        child: Row(children: [Column(
+                                          crossAxisAlignment:
+                                          CrossAxisAlignment.end,
+                                          children: <Widget>[
+                                            Text(
+                                              'نگاهی به عملکرد پلتفرم‌های دیجیتال در سال کابوس‌وار ۲۰۲۱',
+                                              textDirection: TextDirection.rtl,
+                                              textAlign: TextAlign.right,
+                                              style: TextStyle(
+                                                fontFamily: 'tahoma',
+                                                fontSize: 12,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                            SizedBox(
+                                              height: 6,
+                                            ),
+                                            Row(
+                                              mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                              children: [
+                                                Icon(
+                                                  Icons
+                                                      .keyboard_double_arrow_left_rounded,
+                                                  color: Color(0xff337ab7),
+                                                  size: 9,
+                                                ),
+                                                TextButton(
+                                                    onPressed: () {},
+                                                    child: Text(
+                                                      'مشاهده ادامه متن',
+                                                      style: TextStyle(
+                                                        fontFamily: 'tahoma',
+                                                        fontSize: 9,
+                                                        color: Color(0xff337ab7),
+                                                      ),
+                                                    )),
+                                              ],
+                                            )
+                                          ],
+                                        ),
+                                          Container(
+                                            margin: EdgeInsets.all(8),
+                                            width: 250,
+                                            height: 125,
+                                            decoration: BoxDecoration(
+                                                borderRadius:
+                                                BorderRadius.circular(16),
+                                                image: DecorationImage(
+                                                  fit: BoxFit.fitWidth,
+                                                  image: AssetImage(
+                                                    'assets/images/2 (2).png',
+                                                  ),
+                                                )),
+                                          ),],),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  )
                 ],
               )
             ],
